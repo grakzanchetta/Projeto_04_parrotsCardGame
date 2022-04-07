37edc59b0e4350e1c71761507e1b67dd4cf54136
grakzanchetta <grakzanchetta@gmail.com>
2022-04-07 15:42:56 -0300

    Segunda Carta - Primeira situação feita

--- scripts.js
+++ scripts.js
@@ -1,10 +1,14 @@
 document.querySelector("header h1").innerHTML="PARROT CARD GAME" 
 
+const cartas = ['unicornparrot','unicornparrot', 'tripletsparrot', 'tripletsparrot', 'revertitparrot', 'revertitparrot', 'metalparrot', 'metalparrot', 'fiestaparrot', 'fiestaparrot', 'explodyparrot', 'explodyparrot', 'bobrossparrot','bobrossparrot'];
+let numeroCartas = Number(prompt("Escolha o número de cartas que deseja iniciar o jogo. Obs: Apenas valores pares, entre 4 e 14."));
+let baralhoCortado = cartas.slice(0 , numeroCartas);
+let cartasEmbaralhadas = baralhoCortado.sort(comparador);
+let primeiraCarta = null;
+let segundaCarta = null;
+let cartaVirada = null;
+
 function configurarJogo (){
-    cartas = ['unicornparrot','unicornparrot', 'tripletsparrot', 'tripletsparrot', 'revertitparrot', 'revertitparrot', 'metalparrot', 'metalparrot', 'fiestaparrot', 'fiestaparrot', 'explodyparrot', 'explodyparrot', 'bobrossparrot','bobrossparrot'];
-
-    numeroCartas = Number(prompt("Escolha o número de cartas que deseja iniciar o jogo. Obs: Apenas valores pares, entre 4 e 14."));
-
     while (numeroCartas %2 !== 0 || numeroCartas < 4 || numeroCartas > 14){
         numeroCartas = Number(prompt("Escolha o número de cartas que deseja iniciar o jogo. Obs: Apenas valores pares, entre 4 e 14."));
     }
@@ -12,27 +16,47 @@ function configurarJogo (){
 }
 
 function cortarBaralho(numeroCartas){
-    baralhoCortado = cartas.slice(0 , numeroCartas);
     return(baralhoCortado);
 }
 
 function comparador() { 
-        return Math.random() - 0.5; 
-    }
+    return Math.random() - 0.5; 
+}
 
 function embaralharCartas(){
-    cartasEmbaralhadas = baralhoCortado.sort(comparador);
     return (cartasEmbaralhadas);
 }
 
 function montarTabuleiro(cartasEmbaralhadas){
     for (i = 0; i < cartasEmbaralhadas.length; i++){  
-        document.querySelector(".tabuleiro").innerHTML+=`<div class='carta'><img class='traseira-carta' onclick='virarCarta(this)' src='arquivos/front.png'/><img class='frente-carta escondido' src='arquivos/${cartasEmbaralhadas[i]}.gif' /></div>`        }
+        document.querySelector(".tabuleiro").innerHTML+=`<div class='carta' onclick='virarCarta(this)'><img class='traseira-carta'src='arquivos/front.png'/><img class='frente-carta escondido' src='arquivos/${cartasEmbaralhadas[i]}.gif' /></div>`        
+    }
 }    
 
 function virarCarta(cartaVirada){
-        cartaVirada.querySelector(".traseira-carta").add("escondido");
-        cartaVirada.querySelector(".frente-carta").remove("escondido");
+        cartaVirada.querySelector(".carta .traseira-carta").classList.add("escondido");
+        cartaVirada.querySelector(".carta .frente-carta").classList.remove("escondido");
+        if (primeiraCarta === null){
+            primeiraCarta = cartaVirada;
+        } else {
+            segundaCarta = cartaVirada
+        }
+        igualdadeCartas();
+}
+
+function igualdadeCartas (){
+    if (primeiraCarta.innerHTML === segundaCarta.innerHTML){
+        primeiraCarta.classList.add("cartaOK");
+        segundaCarta.classList.add("cartaOK");
+        primeiraCarta.classList.remove("cartaNaoOK");
+        segundaCarta.classList.remove("cartaNaoOK");
+        alert("boa");
+    } else {
+        primeiraCarta.classList.add("cartaNaoOK");
+        segundaCarta.classList.add("cartaNaoOK");
+        primeiraCarta.classList.remove("cartaOK");
+        segundaCarta.classList.remove("cartaOK");
+    }
 }
 
 configurarJogo();
